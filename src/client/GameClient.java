f1b439501d79b14cb128b19c04bfc294987f5920
quentin.rade <quentinrade@gmail.com>
2017-01-18 15:49:53 +0100

    no message

--- src/client/GameClient.java
+++ src/client/GameClient.java
@@ -1,20 +1,38 @@
 package client;
 
-public class GameClient {
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial") // ajout Q
+public class GameClient extends JPanel{
 
 	private MainCanvas canvas;
 
-	public static void main(String[] args) {
-		
-		
+	@Override
+	protected void paintComponent(Graphics g) { // ajout Q
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+		for (int i=0; i<getWidth()/2; i+=10) {
+			g.drawOval(i,  i, getWidth()-i , getHeight()-i);
+		}
+	} // ajout Q
+	public static void main(String[] args) {	
 		
 		GameClient toto = new GameClient();
 		toto.InitMain();
+		
+		JFrame jf=new JFrame("Test"); // ajout Q
+		jf.setContentPane(new GameClient()); // ajout Q
+		jf.setVisible(true); // ajout Q 
 	}
 	
 	public void InitMain(){
 		this.canvas = new MainCanvas();
 		this.canvas.Init();
+		
 	}
 	
 	
