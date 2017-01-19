344d2283877751db5cc72cd67b1f9259b2675acd
Guillaume Courtin <gui.courtin@gmail.com>
2017-01-19 10:21:56 +0100

    add exception

--- src/common/IPlayer.java
+++ src/common/IPlayer.java
@@ -1,13 +1,14 @@
 package common;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 import common.PlayerImpl.Color;
 
 public interface IPlayer extends Remote {
-	public String getName();
+	public String getName() throws RemoteException;
 
-	public Color getColor();
+	public Color getColor() throws RemoteException;
 
-	public void setColor(Color color);
+	public void setColor(Color color) throws RemoteException;
 }
