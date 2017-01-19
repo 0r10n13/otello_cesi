3c92ca2a32997a295655b5895dcf0adef6baba55
Guillaume Courtin <gui.courtin@gmail.com>
2017-01-19 17:29:04 +0100

    fix

--- src/common/IObservable.java
+++ src/common/IObservable.java
@@ -1,9 +1,11 @@
 package common;
 
+import java.rmi.RemoteException;
+
 public interface IObservable {
-	public void addObserver(IObservator observer);
+	public void addObserver(IObservator observer) throws RemoteException;
 	
-	public void deleteObserver(IObservator observer);
+	public void deleteObserver(IObservator observer) throws RemoteException;
 	
-	public void notifyObservers();
+	public void notifyObservers() throws RemoteException;
 }
